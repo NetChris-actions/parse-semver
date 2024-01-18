6d061b25b75f341a5bfb87dfab58ca37bacd7241
Chris Simmons <chris.dev@netchris.com>
2024-01-17 23:02:10 -0800

    Refactor, Rename

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,21 @@
 
 Version="$1"
 
-RegEx="^v?(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
+PureRegEx="^v?(.*)$"
+semVer=$(echo $Version | pcre2grep -o1 $PureRegEx)
 
-all=$(echo $Version | pcre2grep $RegEx)
-major=$(echo $Version | pcre2grep -o1 $RegEx)
-minor=$(echo $Version | pcre2grep -o2 $RegEx)
-patch=$(echo $Version | pcre2grep -o3 $RegEx)
-prerelease=$(echo $Version | pcre2grep -o4 $RegEx)
-buildmetadata=$(echo $Version | pcre2grep -o5 $RegEx)
+# https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+RegEx="^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
 
-echo "all=$all" >> $GITHUB_OUTPUT
-echo "major=$major" >> $GITHUB_OUTPUT
-echo "minor=$minor" >> $GITHUB_OUTPUT
-echo "patch=$patch" >> $GITHUB_OUTPUT
-echo "prerelease=$prerelease" >> $GITHUB_OUTPUT
-echo "buildmetadata=$buildmetadata" >> $GITHUB_OUTPUT
+semVerMajor=$(echo $semVer | pcre2grep -o1 $RegEx)
+semVerMinor=$(echo $semVer | pcre2grep -o2 $RegEx)
+semVerPatch=$(echo $semVer | pcre2grep -o3 $RegEx)
+semVerPreRelease=$(echo $semVer | pcre2grep -o4 $RegEx)
+semVerBuildMetadata=$(echo $semVer | pcre2grep -o5 $RegEx)
+
+echo "semVer=$semVer" >> $GITHUB_OUTPUT
+echo "semVerMajor=$semVerMajor" >> $GITHUB_OUTPUT
+echo "semVerMinor=$semVerMinor" >> $GITHUB_OUTPUT
+echo "semVerPatch=$semVerPatch" >> $GITHUB_OUTPUT
+echo "semVerPreRelease=$semVerPreRelease" >> $GITHUB_OUTPUT
+echo "semVerBuildMetadata=$semVerBuildMetadata" >> $GITHUB_OUTPUT
