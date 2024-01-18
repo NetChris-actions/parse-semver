01ef65ec1605025eac71f877a3c403acc9ac977f
Chris Simmons <chris.dev@netchris.com>
2024-01-17 23:10:37 -0800

    Refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,16 @@
 Version="$1"
 
 PureRegEx="^v?(.*)$"
-semVer=$(echo $Version | pcre2grep -o1 $PureRegEx)
 
 # https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
-RegEx="^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
+SemVer2RegEx="^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$"
 
-semVerMajor=$(echo $semVer | pcre2grep -o1 $RegEx)
-semVerMinor=$(echo $semVer | pcre2grep -o2 $RegEx)
-semVerPatch=$(echo $semVer | pcre2grep -o3 $RegEx)
-semVerPreRelease=$(echo $semVer | pcre2grep -o4 $RegEx)
-semVerBuildMetadata=$(echo $semVer | pcre2grep -o5 $RegEx)
+semVer=$(echo $Version | pcre2grep -o1 $PureRegEx)
+semVerMajor=$(echo $semVer | pcre2grep -o1 $SemVer2RegEx)
+semVerMinor=$(echo $semVer | pcre2grep -o2 $SemVer2RegEx)
+semVerPatch=$(echo $semVer | pcre2grep -o3 $SemVer2RegEx)
+semVerPreRelease=$(echo $semVer | pcre2grep -o4 $SemVer2RegEx)
+semVerBuildMetadata=$(echo $semVer | pcre2grep -o5 $SemVer2RegEx)
 
 echo "semVer=$semVer" >> $GITHUB_OUTPUT
 echo "semVerMajor=$semVerMajor" >> $GITHUB_OUTPUT
