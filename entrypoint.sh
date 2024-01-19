903f7708bde855433b0db26e6bbdd3c53dc4693b
Chris Simmons <chris.dev@netchris.com>
2024-01-18 22:59:32 -0800

    Fix majorMinorOnly, don't return inputValue

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ InputValue="$1"
 
 # Detect a "relaxed" major.minor version from the input
 MajorMinorRegEx="((0|[1-9]\d*)\.(0|[1-9]\d*))"
+majorMinorOnly=$(echo $InputValue | pcre2grep -o1 $MajorMinorRegEx)
 majorMinorOnlyMajor=$(echo $InputValue | pcre2grep -o2 $MajorMinorRegEx)
 majorMinorOnlyMinor=$(echo $InputValue | pcre2grep -o3 $MajorMinorRegEx)
 
@@ -38,10 +39,6 @@ else
   minorVersion=$majorMinorOnlyMinor
 fi
 
-# Convenience construction
-majorMinorOnly="$majorVersion.$minorVersion"
-
-echo "inputValue=$InputValue" >> $GITHUB_OUTPUT
 echo "hasSemVer=$hasSemVer" >> $GITHUB_OUTPUT
 echo "semVer=$semVer" >> $GITHUB_OUTPUT
 echo "majorMinorOnly=$majorMinorOnly" >> $GITHUB_OUTPUT
