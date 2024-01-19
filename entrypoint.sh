e0e16bfb3b7b5c6f3edf9b82e6615203905394a9
Chris Simmons <chris.dev@netchris.com>
2024-01-18 23:29:50 -0800

    Re-put fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,7 @@ fi
 
 # Detect a "relaxed" major.minor version from the input
 MajorMinorRegEx="((0|[1-9]\d*)\.(0|[1-9]\d*))"
+majorMinorOnly=$(echo $InputValue | pcre2grep -o1 $MajorMinorRegEx)
 majorMinorOnlyMajor=$(echo $InputValue | pcre2grep -o2 $MajorMinorRegEx)
 majorMinorOnlyMinor=$(echo $InputValue | pcre2grep -o3 $MajorMinorRegEx)
 
@@ -44,10 +45,6 @@ else
   minorVersion=$majorMinorOnlyMinor
 fi
 
-# Convenience construction
-majorMinorOnly="$majorVersion.$minorVersion"
-
-echo "inputValue=$InputValue" >> $GITHUB_OUTPUT
 echo "hasSemVer=$hasSemVer" >> $GITHUB_OUTPUT
 echo "semVer=$semVer" >> $GITHUB_OUTPUT
 echo "majorMinorOnly=$majorMinorOnly" >> $GITHUB_OUTPUT
