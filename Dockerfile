c3640585d6c52106fb9252885702c97ffd83fb06
Chris Simmons <chris.dev@netchris.com>
2024-01-20 09:05:42 -0800

    Try splitting again

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM alpine:3.10
 
 RUN apk add pcre2-tools
 
+COPY logic.sh /
 COPY entrypoint.sh /
 
 ENTRYPOINT ["/entrypoint.sh"]
---
+++ logic.sh
@@ -0,0 +1,57 @@
+#!/bin/sh -l
+
+value_to_parse="$1"
+
+if [ -z "$value_to_parse" ]
+then
+  echo "No value to parse"
+  return -1
+fi
+
+# Detect a "relaxed" major.minor version from the input
+MajorMinorRegEx="((0|[1-9]\d*)\.(0|[1-9]\d*))"
+major_minor_version=$(echo $value_to_parse | pcre2grep -o1 $MajorMinorRegEx)
+majorMinorOnlyMajor=$(echo $value_to_parse | pcre2grep -o2 $MajorMinorRegEx)
+majorMinorOnlyMinor=$(echo $value_to_parse | pcre2grep -o3 $MajorMinorRegEx)
+
+# https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+# - Modifications from official RegEx: 
+#   - Relaxed from the published RegEx to find it the first time in a line, not requiring that the input be a pure SemVer (i.e. not bookended by beginning and end of line)
+#   - Extract the version only, "trimming off" anything preceding or following it
+SemVer2RegEx="((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)"
+
+semver_version=$(echo $value_to_parse | pcre2grep -o1 $SemVer2RegEx)
+major_version=$(echo $value_to_parse | pcre2grep -o2 $SemVer2RegEx)
+minor_version=$(echo $value_to_parse | pcre2grep -o3 $SemVer2RegEx)
+patch_version=$(echo $value_to_parse | pcre2grep -o4 $SemVer2RegEx)
+pre_release_version=$(echo $value_to_parse | pcre2grep -o5 $SemVer2RegEx)
+build_metadata=$(echo $value_to_parse | pcre2grep -o6 $SemVer2RegEx)
+
+has_semver_version=false
+is_pre_release=false
+
+if [ ! -z "$semver_version" ]
+then
+  has_semver_version=true
+
+  if [ ! -z "$pre_release_version" ]
+  then
+    is_pre_release=true
+  fi
+
+else
+  # If not an official SemVer value, use majorMinorOnlyMajor and majorMinorOnlyMinor for major_version and minor_version respectively
+  major_version=$majorMinorOnlyMajor
+  minor_version=$majorMinorOnlyMinor
+fi
+
+echo "value_to_parse=$value_to_parse"
+echo "has_semver_version=$has_semver_version"
+echo "semver_version=$semver_version"
+echo "major_minor_version=$major_minor_version"
+echo "major_version=$major_version"
+echo "minor_version=$minor_version"
+echo "patch_version=$patch_version"
+echo "pre_release_version=$pre_release_version"
+echo "build_metadata=$build_metadata"
+echo "is_pre_release=$is_pre_release"
